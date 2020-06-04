eaaf4a122b1d98b1063984793743a58165f81f8f
Joshua Gilmore <djsounddog@gmail.com>
2020-06-04 15:50:18 +1000

    junk reference file

---
+++ sptfy.js
@@ -0,0 +1,27 @@
+#!/usr/local/bin/node
+const chalk = require('chalk');
+
+async function authenticate() { }
+
+sptfy()
+
+//
+// user-read-currently-playing user-modify-playback-state user-read-playback-state
+
+// https://accounts.spotify.com/authorize?client_id=7c3e071d9a924d1bb2c9ca3c63b3d963&response_type=token&redirect_uri=http://localhost:3000&scope=app-remote-control&show_dialog=false&state=123
+
+Pause a User's Playback
+Seek To Position In Currently Playing Track
+Set Repeat Mode On User’s Playback
+Set Volume For User's Playback
+Skip User’s Playback To Next Track
+Skip User’s Playback To Previous Track
+Start/Resume a User's Playback
+Toggle Shuffle For User’s Playback
+Transfer a User's Playback
+Add An Item To The End Of User's Current Playback Queue
+
+// console.log(chalk.green("Hello world!"));
+
+
+https://accounts.spotify.com/authorize?client_id=7c3e071d9a924d1bb2c9ca3c63b3d963&redirect_uri=localhost:5500&response_type=token&scope=app-remote-control&show_dialog=false
