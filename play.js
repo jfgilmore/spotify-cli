76294565347a8f5c6a464271f0c88cdd011e319f
Joshua Gilmore <djsounddog@gmail.com>
2020-06-04 22:05:41 +1000

    "Refactored so ALL shared resources are in authentication"

--- play.js
+++ play.js
@@ -1,10 +1,11 @@
+// Import API authentication & functions
 const auth = require("./spotifyAuthentication");
 
 // Method specific constants
 const endpoint = "/v1/me/player/play";
 const method = "PUT";
-const url = auth.apiURI + endpoint;
-const headers = { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + auth.token }
-const init = { method: method, headers: headers };
+const url = auth.apiUri + endpoint;
 
-auth.send(url, init);
+
+auth.send(url, method);
+console.log(auth.chalk.green(`Play`));
--- spotifyAuthentication.js
+++ spotifyAuthentication.js
@@ -1,22 +1,33 @@
 const chalk = require("chalk");
-const fetch  = require("node-fetch");
-const apiURI = "https://api.spotify.com";
-const token = "BQBCxaWbwIc4FXrqQZd6LDVj_gTbPFMLW_slAt4ik-2_Yawsz7JmifeZ-r9qoKyco0SH1O5oOGEgnrJ5NUWxCmhzFQdKlMAqbljNhZVb-720ds5cU_KL0jxhQRp5ZQ_pFHRkl1t0UEniUYzfKxpA3g";
+const fetch = require("node-fetch");
+const apiUri = "https://api.spotify.com";
+const token =
+	"BQAeeL4FfxZTgc6I-EPNz1UCWHzT0uJxa0u2gbq3g6R_taT1ytg2CR2qoReRcUOZOtErwJAuzBie7Uf-8f32sFpYjAfLLxroBTrbN1NiWVQ5HKaced5S23RNrhXlffHuxuvdmshDMb1875tDjQrEuQ";
+const statusEndpoint = "/v1/me/player";
+// const statusUrl = apiUri + statusEndpoint;
+// const statusMethod = "GET";
+// const status = send(statusUrl, statusMethod);
+const headers = {
+	"Content-Type": "application/json",
+	Authorization: "Bearer " + token,
+};
 
-const send = async (url, init) => {
-  try {
-    const response = await fetch(url, init);
-    const status = response.status;
-    if (status === 401) { throw "You need to re-authenticate!" }
-    console.log(response);
-    console.log(chalk.green(`Playing on ${}`));
-  } catch (error) {
-    console.log(error);
-  }
+const send = async (url, method) => {
+	try {
+		const init = { method: method, headers: headers };
+		const response = await fetch(url, init);
+		const status = response.status;
+		if (status === 401) {
+			throw status;
+		}
+	} catch (error) {
+    // Authentication function
+		console.log(error);
+	}
 };
 
 module.exports = {
-  apiURI,
+	apiUri,
   send,
-	token
+	chalk,
 };
